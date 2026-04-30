7413261ca73a71d4cc9c6e741998e90c988b3778
Kadir Yönak <128465120+kadiryonak@users.noreply.github.com>
2026-05-01 01:04:35 +0300

    Delete src/preprocess/selector.py

--- src/preprocess/selector.py
+++
@@ -1,6 +0,0 @@
-"""
-Karmaşıklığa göre en kritik metotları seçen modül.
-
-Metotları (cyclomatic + cognitive) toplam skoruna göre büyükten küçüğe sıralar
-ve en karmaşık N metodu seçer. Varsayılan limit: 50.
-"""
